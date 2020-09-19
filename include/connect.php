e98ff4a655d8d2414876a1106292a590b69b6bb5
ESTA-2509 <65528523+ESTA-2509@users.noreply.github.com>
2020-09-20 00:53:13 +0700

    Update connect.php

--- include/connect.php
+++ include/connect.php
@@ -1,4 +1,4 @@
 <?php
-	$link=mysqli_connect("localhost","root","","dienthoai") or die("Cannot connect to the localhost");
+	$link=mysqli_connect("localdb","azure","6#vWHD_$","dienthoai") or die("Cannot connect to the localhost");
 	$link -> set_charset("utf8");
 ?>
