9632a240a4582cac295e073fa998aa48f367261e
ESTA-2509 <65528523+ESTA-2509@users.noreply.github.com>
2020-09-20 00:19:52 +0700

    Update connect.php

--- include/connect.php
+++ include/connect.php
@@ -1,4 +1,17 @@
 <?php
-	$link=mysqli_connect("localhost","root","","dienthoai") or die("Cannot connect to the localhost");
+	// PHP Data Objects(PDO) Sample Code:
+try {
+    $link = new PDO("sqlsrv:server = tcp:dienthoai.database.windows.net,1433; Database = dienthoai", "gr0062", "{your_password_here}");
+    $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "gr0062", "pwd" => "{your_password_here}", "Database" => "dienthoai", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:dienthoai.database.windows.net,1433";
+$link = sqlsrv_connect($serverName, $connectionInfo);
 	$link -> set_charset("utf8");
 ?>
