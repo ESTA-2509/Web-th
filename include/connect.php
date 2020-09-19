12a4cc8ab1ee0fdb014fdc99b8071d59eeec474e
ESTA-2509 <65528523+ESTA-2509@users.noreply.github.com>
2020-09-20 00:47:35 +0700

    Update connect.php

--- include/connect.php
+++ include/connect.php
@@ -1,17 +1,4 @@
 <?php
-	// PHP Data Objects(PDO) Sample Code:
-try {
-    $link = new PDO("sqlsrv:server = tcp:dienthoai.database.windows.net,1433; Database = dienthoai", "gr0062", "{Hung*2509}");
-    $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
-
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "gr0062", "pwd" => "{Hung*2509}", "Database" => "dienthoai", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:dienthoai.database.windows.net,1433";
-$link = sqlsrv_connect($serverName, $connectionInfo);
+	$link=mysqli_connect("localhost","root","","dienthoai") or die("Cannot connect to the localhost");
 	$link -> set_charset("utf8");
 ?>
